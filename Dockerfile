849df08b6f9b1c9fc44ba668aa9785cf5bb3e30a
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2023-11-28 10:27:44 +0100

    Fixed config file

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,14 @@ RUN mkdir /app
 
 # Copia il tuo file ZIP nell'immagine
 COPY ./app/build/distributions/app.zip /app
+COPY ./app/src/main/resources/config_pa.yml app/app/config/config_pa.yml
+COPY ./app/src/main/resources/config_da.yml app/app/config/config_da.yml
 
 # Sposta il file ZIP nella directory /app
 WORKDIR /app
 RUN unzip app.zip -d app
 
-EXPOSE 1883
+#EXPOSE 1886
 #L'istruzione EXPOSE <port> informa Docker che il container sarà in ascolto sulle porte specificate;
 #documenta che le porte indicate sono disponibili alla pubblicazione, ma non le pubblica.
 #Solo con il comando docker run -p 8080:8080 applicazione_prova mappiamo effettivamente la porta 8080 del container
--- app/src/main/java/demomqttnewadapters/App.java
+++ app/src/main/java/demomqttnewadapters/App.java
@@ -8,31 +8,78 @@ import it.wldt.exception.EventBusException;
 import it.wldt.exception.ModelException;
 import it.wldt.exception.WldtConfigurationException;
 import it.wldt.exception.WldtRuntimeException;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import physical.MqttPhysicalAdapter;
 import physical.MqttPhysicalAdapterConfiguration;
 import physical.exception.MqttPhysicalAdapterConfigurationException;
 
-import javax.swing.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class App {
     public static void main(String[] args) throws WldtConfigurationException, ModelException, WldtRuntimeException, EventBusException, MqttPhysicalAdapterConfigurationException, MqttException, MqttDigitalAdapterConfigurationException, IOException {
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "error");
 
         WldtEngine dtEngine = new WldtEngine(new DefaultShadowingFunction(), "mqtt-device-digital-twin");
-        dtEngine.addPhysicalAdapter(buildMqttPhysicalAdapter("app/src/main/resources/config_pa.yml"));
-        dtEngine.addDigitalAdapter(buildMqttDigitalAdapter("app/src/main/resources/config_da.yml"));
+        dtEngine.addPhysicalAdapter(buildMqttPhysicalAdapter("/app/app/config/config_pa.yml"));
+        dtEngine.addDigitalAdapter(buildMqttDigitalAdapter("/app/app/config/config_da.yml"));
+
+        /*App app = new App();
+        String fileName = "resources/config_pa.yml";
+
+        System.out.println("getResourceAsStream : " + fileName);
+        InputStream is = app.getFileFromResourceAsStream(fileName);
+        printInputStream(is);*/
+
 
         dtEngine.startLifeCycle();
+    }
+
+    private InputStream getFileFromResourceAsStream(String fileName) {
+
+        // The class loader that loaded the class
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        // the stream holding the file content
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
+
+    }
+
+    private static void printInputStream(InputStream is) {
+
+        try (InputStreamReader streamReader =
+                     new InputStreamReader(is, StandardCharsets.UTF_8);
+             BufferedReader reader = new BufferedReader(streamReader)) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
 
     private static MqttPhysicalAdapter buildMqttPhysicalAdapter(String filepath) throws MqttPhysicalAdapterConfigurationException, MqttException, IOException {
         MqttPhysicalAdapterConfiguration configuration = MqttPhysicalAdapterConfiguration
-                .builder(filepath)
+                .builder(filepath)//"host.docker.internal", 1883)
                 .readFromConfig()
+                //.addPhysicalAssetPropertyAndTopic("intensity", 0, "sensor/intensity", Integer::parseInt)
+                //.addPhysicalAssetActionAndTopic("switch-off", "sensor.actuation", "text/plain", "sensor/actions/switch", actionBody -> "switch" + actionBody)
                 .build();
         return new MqttPhysicalAdapter("mqtt-device-pa", configuration);
     }
@@ -41,8 +88,10 @@ public class App {
         //NB: In MqttDigitalAdapter topics for Property and Event are OutgoingTopics,
         // so each function takes as input something that has the same type as the body of the Event or Property
         MqttDigitalAdapterConfiguration configuration = MqttDigitalAdapterConfiguration
-                .builder(filepath)
+                .builder(filepath)//"host.docker.internal", 1883)
                 .readFromConfig()
+                //.addActionTopic("switch_off", "app/actions/switch-off", msg -> "OFF")
+                //.addActionTopic("switch_on", "app/actions/switch-on", msg -> "ON")
                 .build();
         return new MqttDigitalAdapter("test-mqtt-da", configuration);
     }
